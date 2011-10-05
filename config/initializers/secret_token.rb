0fc2282ca215ec169e79244cd3b508ce0479382e
Khaled alHabache <khellls@gmail.com>
2011-10-05 07:26:02 +0300

    secure tokens

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ContestApp::Application.config.secret_token = '4c16b2d0ac2ab5ace3b501ee103884258bd36bdfe02aaf0833352488bded20f6b9cb1b812a65ad6b99fefd4added7544e1b10ba4543f0e2218408828b552ca0e'
+ContestApp::Application.config.secret_token = APP_CONFIG[:secret_token]
+
